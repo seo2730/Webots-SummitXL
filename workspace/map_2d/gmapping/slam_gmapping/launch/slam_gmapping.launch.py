f579c0c7ba0169088a14d06fd7c6e0591c739e09
seo2730 <seo2730@naver.com>
2026-04-11 07:00:25 +0900

    clock 추가 및 파라미터 조정

--- workspace/map_2d/gmapping/slam_gmapping/launch/slam_gmapping.launch.py
+++ workspace/map_2d/gmapping/slam_gmapping/launch/slam_gmapping.launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'namespace',
-            default_value='',
+            default_value='ugv1',
             description='Top-level namespace'
         ),
         IncludeLaunchDescription(
@@ -29,7 +29,13 @@ def generate_launch_description():
             name='slam_gmapping',
             namespace=namespace, # 2. Node에 namespace 전달 (필수)
             output='screen',
-            parameters=[{'use_sim_time': True}],
+            parameters=[{
+                        'use_sim_time': True,
+                        # Gmapping에게 네임스페이스가 붙은 TF 프레임 이름을 명시적으로 알려줍니다.
+                        'base_frame': [namespace, '/base_link'], # 결과: ugv1/base_link
+                        'odom_frame': [namespace, '/odom'],      # 결과: ugv1/odom
+                        'map_frame': 'map'  # 공유 맵을 쓴다면 'map', 아니면 [namespace, '/map']}],
+                     }],     
             # 3. 리매핑 시 반드시 앞쪽에 '/'가 없는 상대 경로('scan', 'map')를 사용해야 합니다.
             remappings=[
                 ('/scan', 'scan'),   # 노드 내부에서 절대경로를 쓴다면 상대경로로 강제 리매핑
--- workspace/simulator/simulator/robot_driver.py
+++ workspace/simulator/simulator/robot_driver.py
@@ -1,20 +1,24 @@
 import rclpy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-from math import pi, atan2
 from rosgraph_msgs.msg import Clock
+import math
 
+from tf2_ros import TransformBroadcaster
+from geometry_msgs.msg import TransformStamped
+
+from rclpy.parameter import Parameter
+from builtin_interfaces.msg import Time
 
 HALF_DISTANCE_BETWEEN_WHEELS = 0.045
 WHEEL_RADIUS = 0.123
-LX = 0.2045 #lateral distance from robot's COM to wheel [m].
-LY = 0.2225 #longitudinal distance from robot's COM to wheel [m].
+LX = 0.2045 
+LY = 0.2225 
 target_speed = {'x': 1.0, 'y': 1.0, 'z': 1.0}
-
 
 class main:
     def init(self, webots_node, properties):
-        print("===== init() called =====", flush=True)  # 추가
+        print("===== init() called =====", flush=True)
         self.__robot = webots_node.robot
         self.__timestep = int(self.__robot.getBasicTimeStep())
 
@@ -22,26 +26,41 @@ class main:
         self.fr_motor = self.__robot.getDevice('front_right_wheel_joint')
         self.bl_motor = self.__robot.getDevice('back_left_wheel_joint')
         self.br_motor = self.__robot.getDevice('back_right_wheel_joint')
+        
         self.fl_motor.setPosition(float('inf'))
-        self.fl_motor.setVelocity(0)
+        self.fl_motor.setVelocity(0.0)
         self.fr_motor.setPosition(float('inf'))
-        self.fr_motor.setVelocity(0)
+        self.fr_motor.setVelocity(0.0)
         self.bl_motor.setPosition(float('inf'))
-        self.bl_motor.setVelocity(0)
+        self.bl_motor.setVelocity(0.0)
         self.br_motor.setPosition(float('inf'))
-        self.br_motor.setVelocity(0)
+        self.br_motor.setVelocity(0.0)
 
-        # 이미 init됐으면 다시 하지 않음
         if not rclpy.ok():
             rclpy.init(args=None)
 
-        self.__node = rclpy.create_node('robot_driver')
-        # properties에서 네임스페이스 받기
-        namespace = properties.get('namespace', '')
-        topic = f'{namespace}/cmd_vel' if namespace else 'cmd_vel'
+        self.namespace = properties.get('namespace', '')
 
-        self.__node.create_subscription(Twist, topic, self.__cmd_vel_callback, 1)
-        self.__target_twist = Twist()  # ✅ 추가
+        self.__node = rclpy.create_node(
+            'robot_driver',
+            namespace=self.namespace,
+            parameter_overrides=[Parameter('use_sim_time', Parameter.Type.BOOL, True)]
+        )
+
+        self.odom_publisher = self.__node.create_publisher(Odometry, 'odom', 10)
+        self.__node.create_subscription(Twist, 'cmd_vel', self.__cmd_vel_callback, 1)
+        self.__target_twist = Twist()
+        self.__tf_broadcaster = TransformBroadcaster(self.__node)
+        self.clock_publisher = self.__node.create_publisher(Clock, '/clock', 10)
+
+        self.gps = self.__robot.getDevice('gps')
+        if self.gps:
+            self.gps.enable(self.__timestep)
+
+        self.imu = self.__robot.getDevice('imu')
+        if self.imu:
+            self.imu.enable(self.__timestep)
+
         print("===== init() done =====", flush=True)
 
     def __cmd_vel_callback(self, twist):
@@ -49,6 +68,15 @@ class main:
 
     def step(self):
         rclpy.spin_once(self.__node, timeout_sec=0)
+
+        # Webots 시간 먼저 가져오기
+        wb_time = self.__robot.getTime()
+
+        # clock publish
+        clock_msg = Clock()
+        clock_msg.clock.sec = int(wb_time)
+        clock_msg.clock.nanosec = int((wb_time - int(wb_time)) * 1e9)
+        self.clock_publisher.publish(clock_msg)
 
         vx = self.__target_twist.linear.x
         vy = self.__target_twist.linear.y
@@ -61,10 +89,47 @@ class main:
         self.br_motor.setVelocity(bl)
         self.fr_motor.setVelocity(br)
 
+        if self.gps and self.imu:
+            gps_vals = self.gps.getValues()
+            
+            if gps_vals and not math.isnan(gps_vals[0]):
+                curr_time = Time()
+                curr_time.sec = int(wb_time)
+                curr_time.nanosec = int((wb_time - int(wb_time)) * 1e9)
+
+                t = TransformStamped()
+                t.header.stamp = curr_time
+                t.header.frame_id = 'odom'
+                t.child_frame_id = 'base_link'
+
+                t.transform.translation.x = float(gps_vals[0])
+                t.transform.translation.y = float(gps_vals[1])
+                t.transform.translation.z = float(gps_vals[2])
+
+                rpy = self.imu.getRollPitchYaw()
+                if rpy and not math.isnan(rpy[2]):
+                    yaw = rpy[2]
+                    t.transform.rotation.z = math.sin(yaw / 2.0)
+                    t.transform.rotation.w = math.cos(yaw / 2.0)
+
+                    odom_msg = Odometry()
+                    odom_msg.header.stamp = curr_time
+                    odom_msg.header.frame_id = 'odom'
+                    odom_msg.child_frame_id = 'base_link'
+
+                    odom_msg.pose.pose.position.x = t.transform.translation.x
+                    odom_msg.pose.pose.position.y = t.transform.translation.y
+                    odom_msg.pose.pose.position.z = t.transform.translation.z
+                    
+                    odom_msg.pose.pose.orientation.z = t.transform.rotation.z
+                    odom_msg.pose.pose.orientation.w = t.transform.rotation.w
+
+                    self.odom_publisher.publish(odom_msg)
+                    self.__tf_broadcaster.sendTransform(t)
 
 def mecanumControl(vx, vy, wz):
-    fl = 1 / WHEEL_RADIUS * (vx - vy - ((LY + LY) * wz))
-    fr = 1 / WHEEL_RADIUS * (vx + vy - ((LY + LY) * wz))
-    bl = 1 / WHEEL_RADIUS * (vx + vy + ((LY + LY) * wz))
-    br = 1 / WHEEL_RADIUS * (vx - vy + ((LY + LY) * wz))
+    fl = 1 / WHEEL_RADIUS * (vx - vy - ((LY + LX) * wz))
+    fr = 1 / WHEEL_RADIUS * (vx + vy - ((LY + LX) * wz))
+    bl = 1 / WHEEL_RADIUS * (vx + vy + ((LY + LX) * wz))
+    br = 1 / WHEEL_RADIUS * (vx - vy + ((LY + LX) * wz))
     return fl, fr, bl, br
